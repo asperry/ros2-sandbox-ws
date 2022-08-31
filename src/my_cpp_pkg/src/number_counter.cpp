fd78561809da51a86c81f9860ba97dfd5dc80d2b
Adam Sperry <adam.sperry@outlook.com>
2022-08-30 18:46:52 -0600

    Changes for the services activity from Udemy.

--- src/my_cpp_pkg/src/number_counter.cpp
+++ src/my_cpp_pkg/src/number_counter.cpp
@@ -3,7 +3,9 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
+#include "example_interfaces/srv/set_bool.hpp"
 
+using namespace std::placeholders;
 
 class NumberCounterNode : public rclcpp::Node
 {
@@ -13,6 +15,8 @@ public:
         subscription_ = create_subscription<example_interfaces::msg::Int64>(
             "number", 10, std::bind(&NumberCounterNode::callback_number, this, std::placeholders::_1));
         publisher_ = create_publisher<example_interfaces::msg::Int64>("number_count", 10);
+        reset_counter_service_ = create_service<example_interfaces::srv::SetBool>(
+            "reset_counter", std::bind(&NumberCounterNode::callback_reset_counter, this, _1, _2));
         RCLCPP_INFO(get_logger(), "Number counter has been started.");
     }
 
@@ -25,9 +29,28 @@ private:
         publisher_->publish(new_msg);
     }
 
+    void callback_reset_counter(const example_interfaces::srv::SetBool::Request::SharedPtr request,
+                                example_interfaces::srv::SetBool::Response::SharedPtr response)
+    {
+        if (request->data)
+        {
+            counter_ = 0;
+            response->success = true;
+            response->message = "Counter successfully reset";
+            RCLCPP_INFO(get_logger(), "Counter has been reset");
+        }
+        else
+        {
+            response->success = false;
+            response->message = "Counter has not been reset";
+            RCLCPP_INFO(get_logger(), "Counter has not been reset");
+        }
+    }
+
     int counter_;
     rclcpp::Subscription<example_interfaces::msg::Int64>::SharedPtr subscription_;
     rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher_;
+    rclcpp::Service<example_interfaces::srv::SetBool>::SharedPtr reset_counter_service_;
 };
 
 
--- src/my_py_pkg/my_py_pkg/number_counter.py
+++ src/my_py_pkg/my_py_pkg/number_counter.py
@@ -3,6 +3,7 @@ import rclpy
 from rclpy.node import Node
 
 from example_interfaces.msg import Int64
+from example_interfaces.srv import SetBool
 
 
 class NumberCounterNode(Node):
@@ -10,16 +11,30 @@ class NumberCounterNode(Node):
     def __init__(self):
         super().__init__("number_counter")
         self.counter_ = 0
-        self.subscription_ = self.create_subscription(
+        self.number_subscription_ = self.create_subscription(
             Int64, "number", self.callback_number, 10)
-        self.publisher_ = self.create_publisher(
+        self.number_count_publisher_ = self.create_publisher(
             Int64, "number_count", 10)
+        self.reset_counter_service_ = self.create_service(
+            SetBool, "reset_counter", self.callback_reset_counter)
         self.get_logger().info("Number counter has been started.")
 
     def callback_number(self, msg):
         self.counter_ += msg.data
         msg.data = self.counter_
-        self.publisher_.publish(msg)
+        self.number_count_publisher_.publish(msg)
+
+    def callback_reset_counter(self, request, response):
+        if request.data:
+            self.counter_ = 0
+            response.success = True
+            response.message = "Counter successfully reset"
+            self.get_logger().info("Counter has been reset")
+        else:
+            response.success = False
+            response.message = "Counter has not been reset"
+            self.get_logger().info("Counter has not been reset")
+        return response
 
 
 def main(args=None):
